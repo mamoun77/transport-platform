14b9af89f04e53157cacfc313f93ea2f723f44a1
Your Name <you@example.com>
2026-05-31 13:32:01 +0100

    Set default locale to English (en) in next-i18next config

--- frontend/next-i18next.config.js
+++ frontend/next-i18next.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = {
   i18n: {
-    defaultLocale: 'fr',
-    locales: ['fr', 'en'],
+    defaultLocale: 'en',
+    locales: ['en', 'fr'],
   },
   localePath: path.resolve('./public/locales'),
   reloadOnPrerender: process.env.NODE_ENV === 'development',
